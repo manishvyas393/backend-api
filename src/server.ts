e1556069d5b17b0f4b0716d9f8f126cb2bf2872e
manish vyas <manishvyas393@gmail.com>
2024-08-30 16:03:37 +0530

    typescript api

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import express from "express"
+import bodyParser from "body-parser"
+import cors from "cors"
 const app = express()
-const port=8080
-
+const port = 8080
+app.use(cors())
+app.use(bodyParser.json())
 app.listen(port, () => console.log(`server running on ${port}`))
