97f854ffeb4ee9a5ed139bbb0c0a67db863d432e
manish vyas <manishvyas393@gmail.com>
2024-08-30 15:54:06 +0530

    setup in typescript

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-const express = require("express")
+import express from "express"
 const app = express()
-const port = 8080
+const port=8080
 
 app.listen(port, () => console.log(`server running on ${port}`))
