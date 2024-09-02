01fb0dbb642445f18758a2421cc93c8cd8d5ba4f
manish vyas <manishvyas393@gmail.com>
2024-09-02 13:26:07 +0530

    mofify schema

--- src/models/CommentsSchema.ts
+++ src/models/CommentsSchema.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const CommentsModel = new mongoose.Schema({
       taskId: { type: mongoose.Schema.Types.ObjectId, ref: 'Tasks' },
-      userId: { type: mongoose.Schema.Types.ObjectId, ref: 'Users' },  // user who commented
+      userId: { type: mongoose.Schema.Types.ObjectId, ref: 'Users' },
       comment: String,  // the comment text
 }, { timestamps: true })
 
